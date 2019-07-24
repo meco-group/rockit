c2d946535a35b0701970b55bfe2f3fc540478721
Joris Gillis <joris.gillis42@gmail.com>
2019-07-24 10:58:36 +0200

    issue #61: More elaborate unittests

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -5,29 +5,39 @@ from ocpx import *
 class MiscTests(unittest.TestCase):
 
     def test_basic(self):
-      ocp = OcpMultiStage()
-      stage = ocp.stage(t0=0,T=1)
 
-      x = stage.state()
-      u = stage.control()
+      for T in [1,3.2]:
+        for M in [1,2]:
+          for b in [1,2]:
+            for t0 in [0,1]:
+              for x0 in [0,1]:
+                ocp = OcpMultiStage()
+                stage = ocp.stage(t0=t0,T=T)
 
-      stage.set_der(x,u)
+                x = stage.state()
+                u = stage.control()
 
-      stage.subject_to(u<=1)
-      stage.subject_to(-1<=u)
-      
-      stage.add_objective(stage.at_tf(x))
-      stage.subject_to(stage.at_t0(x)==0)
+                stage.set_der(x,u)
 
-      ocp.method(DirectMethod(solver='ipopt'))
+                stage.subject_to(u<=b)
+                stage.subject_to(-b<=u)
+                
+                stage.add_objective(stage.at_tf(x))
+                stage.subject_to(stage.at_t0(x)==x0)
 
-      stage.method(MultipleShooting(N=4,intg='rk'))
-      
-      sol = ocp.solve()
+                ocp.method(DirectMethod(solver='ipopt'))
 
-      ts, xs = sol.sample(stage,x,grid=stage.grid_control)
+                stage.method(MultipleShooting(N=4,M=M,intg='rk'))
+                
+                sol = ocp.solve()
 
-      self.assertAlmostEqual(xs[-1],-1)
+                ts, xs = sol.sample(stage,x,grid=stage.grid_control)
+
+                self.assertAlmostEqual(xs[0],x0,places=6)
+                self.assertAlmostEqual(xs[-1],x0-b*T,places=6)
+                self.assertAlmostEqual(ts[0],t0)
+                self.assertAlmostEqual(ts[-1],t0+T)
+
 
 if __name__ == '__main__':
     unittest.main()
