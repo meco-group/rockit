24540a0dae0dd07f152c3c1d5330ebfeae813d68
Joris Gillis <joris.gillis42@gmail.com>
2019-07-24 10:47:18 +0200

    Make MultipleShooting define a control grid for ocpx_solution

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -4,9 +4,30 @@ from ocpx import *
 
 class MiscTests(unittest.TestCase):
 
-    def test_stage_constr(self):
+    def test_basic(self):
       ocp = OcpMultiStage()
-      stage = ocp.stage(t0=0,T=10)
+      stage = ocp.stage(t0=0,T=1)
+
+      x = stage.state()
+      u = stage.control()
+
+      stage.set_der(x,u)
+
+      stage.subject_to(u<=1)
+      stage.subject_to(-1<=u)
+      
+      stage.add_objective(stage.at_tf(x))
+      stage.subject_to(stage.at_t0(x)==0)
+
+      ocp.method(DirectMethod(solver='ipopt'))
+
+      stage.method(MultipleShooting(N=4,intg='rk'))
+      
+      sol = ocp.solve()
+
+      ts, xs = sol.sample(stage,x,grid=stage.grid_control)
+
+      self.assertAlmostEqual(xs[-1],-1)
 
 if __name__ == '__main__':
     unittest.main()
