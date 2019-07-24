44aeb0e04d98bae4dacead411f56dba617ad62a7
jeroendm <jeroendemaeyer@live.be>
2019-07-24 20:49:39 +0200

    add docstrings to ocpx_solution and stage_options

--- ocpx/ocpx_solution.py
+++ ocpx/ocpx_solution.py
@@ -1,22 +1,35 @@
-from .stage_options import GridControl, GridIntegrator
 import numpy as np
 from casadi import vertcat
+from .stage_options import GridControl, GridIntegrator
 
 
 class OcpxSolution:
-    def __init__(self, sol):
-        self.sol = sol
+    def __init__(self, nlpsol):
+        """Wrap casadi.nlpsol to simplify access to numerical solution."""
+        self.sol = nlpsol
 
     def sample(self, stage, expr, grid):
+        """Sample expression at solution on a given grid.
+
+        Returns a numpy.array with the numerical values of the
+        expression at the grid points specifeid by the grid type.
+
+        arguments:
+        state -- an optimal control problem stage
+        expr  -- arbitrary expression containing states, controls, ...
+        grid  -- type of time grid to use for sampling,
+        options are available in ocpx.stage_options
+        """
         if isinstance(grid, GridControl):
             return self._grid_control(stage, expr, grid)
 
         elif isinstance(grid, GridIntegrator):
             return self._grid_integrator(stage, expr, grid)
         else:
-            raise Exception("Unknown grid option")
+            raise Exception("Unknown grid option: {}".format(grid))
 
     def _grid_control(self, stage, expr, grid):
+        """Evaluate expression at (N + 1) control points."""
         sub_expr = []
         for k in range(stage._method.N):
             sub_expr.append(stage._constr_apply(
@@ -28,6 +41,7 @@ class OcpxSolution:
         return time, np.array(res)
 
     def _grid_integrator(self, stage, expr, grid):
+        """Evaluate expression at (N*M + 1) integrator discretization points."""
         sub_expr = []
         for k in range(stage._method.N):
             for l in range(stage._method.M):
