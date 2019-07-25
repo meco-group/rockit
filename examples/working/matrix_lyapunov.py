c7d43f661f8542862f70e7c11cabaeae82943fc5
Taranjitsingh Singh <u0113360@kuleuven.be>
2019-07-25 11:31:18 +0200

    stochiastic example using Lyapunov

--- examples/working/matrix_lyapunov.py
+++ examples/working/matrix_lyapunov.py
@@ -23,11 +23,11 @@ stage.set_der(P,A@P+P@A.T)
 
 stage.subject_to(stage.at_t0(x) == vertcat(0.5,0))
 stage.subject_to(stage.at_t0(P)==diag([0.01**2,0.1**2]))
-stage.subject_to(u <= 50)
-stage.subject_to(u >= -50)
-bound = 2 + 0.1*cos(10*stage.t)
+stage.subject_to(u <= 40)
+stage.subject_to(u >= -40)
+bound = lambda t: 2 + 0.1*cos(10*t)
 stage.subject_to(x[0] >= -0.25)
-stage.subject_to(x[0] <= bound)
+stage.subject_to(x[0] <= bound(stage.t))
 stage.add_objective(stage.integral(sumsqr(x[0]-3)))
 
 ocp.method(DirectMethod(solver='ipopt'))
@@ -35,3 +35,13 @@ ocp.method(DirectMethod(solver='ipopt'))
 # Make it concrete for this stage
 stage.method(MultipleShooting(N=40, M=6, intg='rk'))
 sol = ocp.solve()
+
+ts, xsol = sol.sample(stage, x[0], grid=stage.grid_control)
+
+plt.plot(ts, bound(ts), '-o')
+#
+# plt.plot(ts,xsol,'-o')
+#ts,xsol = sol.sample(stage,x2,grid=stage.grid_integrator)
+plt.legend(["x1"])
+
+plt.show()
