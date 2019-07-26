0f035b0b5c56c7c552cb27740e15c39643d88a2e
Joris Gillis <joris.gillis42@gmail.com>
2019-07-26 15:21:04 +0200

    cleanup

--- ocpx/multiple_shooting.py
+++ ocpx/multiple_shooting.py
@@ -49,8 +49,8 @@ class MultipleShooting(SamplingMethod):
                 opti.subject_to(self.eval_at_control(stage, c, k))
         
         for c in stage._path_constraints_expr():  # for each constraint expression
-                # Add it to the optimizer, but first make x,u concrete.
-          opti.subject_to(self.eval_at_control(stage, c, -1))
+            # Add it to the optimizer, but first make x,u concrete.
+            opti.subject_to(self.eval_at_control(stage, c, -1))
 		
         self.xk.append(self.X[-1])
 
