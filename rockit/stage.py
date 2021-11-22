0ee2b70cebe9168e149f40ccfe490743cde1d151
Joris Gillis <joris.gillis42@gmail.com>
2021-11-22 09:29:33 +0100

    fix typo

--- rockit/stage.py
+++ rockit/stage.py
@@ -171,7 +171,7 @@ class Stage:
         """Create a state.
         You must supply a derivative for the state with :obj:`~rockit.stage.Stage.set_der`
 
-        Parameterrs
+        Parameters
         ----------
         n_rows : int, optional
             Number of rows
