d15f71d78ceda78c86a6a06c409d699ef720e5b8
Bastiaan Vandewal <bastiaan.vandewal@kuleuven.be>
2022-12-05 16:15:13 +0100

    Improved documentation for tunnel example

--- examples/motion_planning_tunnel.py
+++ examples/motion_planning_tunnel.py
@@ -16,7 +16,8 @@
 #
 #     You should have received a copy of the GNU Lesser General Public
 #     License along with CasADi; if not, write to the Free Software
-#     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+#     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#     MA  02110-1301  USA
 #
 #
 
@@ -24,25 +25,27 @@
 Motion planning
 ===============
 
-Simple motion planning with circular obstacle
+Simple motion planning within corridor (tunnel)
 """
 
 from rockit import *
 import matplotlib.pyplot as plt
 import numpy as np
 from numpy import pi, cos, sin, tan
-from casadi import vertcat, sumsqr, interpolant
+from casadi import vertcat, sumsqr, interpolant, DM
+from pylab import *
 
+# Fixed time problem (go as far as possible within 10s)
 ocp = Ocp(T=10)
+N = 20
 
 # Bicycle model
-
-x     = ocp.state()
-y     = ocp.state()
+x = ocp.state()
+y = ocp.state()
 theta = ocp.state()
 
 delta = ocp.control()
-V     = ocp.control()
+V = ocp.control()
 
 L = 1
 
@@ -51,91 +54,125 @@ ocp.set_der(y, V*sin(theta))
 ocp.set_der(theta, V/L*tan(delta))
 
 # Initial constraints
-ocp.subject_to(ocp.at_t0(x)==0)
-ocp.subject_to(ocp.at_t0(y)==0)
-ocp.subject_to(ocp.at_t0(theta)==pi/2)
+ocp.subject_to(ocp.at_t0(x) == 0)
+ocp.subject_to(ocp.at_t0(y) == 0)
+ocp.subject_to(ocp.at_t0(theta) == pi/2)
 
-# Final constraint
-#ocp.subject_to(ocp.at_tf(x)==0)
-#ocp.subject_to(ocp.at_tf(y)==10)
+# Initial guesses for states
+x_init = vertcat(np.linspace(0, -2, int(N/2)+1), np.linspace(-2, 0, int(N/2)))
+y_init = np.linspace(0, 10, N+1)
+theta_init = np.linspace(pi/2, pi/2, N+1)
+ocp.set_initial(x, x_init)  # centerline of tunnel
+ocp.set_initial(y, y_init)  # centerline of tunnel
+ocp.set_initial(theta, theta_init)
 
-ocp.set_initial(x,0)
-ocp.set_initial(y,ocp.t)
-ocp.set_initial(theta, pi/2)
-ocp.set_initial(V,1)
+# Initial guesses for controls
+ocp.set_initial(delta, 0)
+ocp.set_initial(V, 1)
 
-ocp.subject_to(0 <= (V<=1))
-ocp.subject_to( -pi/6 <= (delta<= pi/6))
+# Path constraints
+ocp.subject_to(0 <= (V <= 1))
+ocp.subject_to(-pi/6 <= (delta <= pi/6))
 
-sdot = ocp.control()
-
+# Define a progress variable s as a state. The derivative of this state, sdot,
+# is defined as a control input. sdot must be larger than zero in order to
+# only have an increasing s (only positive progress along s). Within two
+# consecutive values, s might not change too much to avoid going out of the
+# predefined circles (upper constraint on sdot).
 s = ocp.state()
-
+sdot = ocp.control()
 ocp.set_der(s, sdot)
-ocp.subject_to(sdot>=0)
+ocp.subject_to(sdot >= 0)
+ocp.subject_to(sdot <= 0.5)
 
+# Define a grid for s on which you will define values for the tunnel (and
+# extend it to a high value since we do not know yet which value s will take
+# at_tf, might be above 5).
+s_grid = [0, 1, 2, 3, 4, 5, 1000]
 
-tunnel_s = [0,1,2,3,4,5,1000]
-tunnel_x = [0,0,-2,-2,0,0,0]
-tunnel_y = [0,2,4,6,8,10,1000]
-tunnel_r = [2,2,1,1,2,2,2]
+# Define values for x, y and radius of the tunnel along the grid s.
+x_grid = [0, 0, -2, -2, 0, 0, 0]
+y_grid = [0, 2, 4, 6, 8, 10, 1000]
+r_grid = [2, 2, 1, 1, 2, 2, 2]
 
-N = 20
-ocp.set_initial(s, np.linspace(tunnel_s[0],tunnel_s[-2], N))
-ocp.set_initial(sdot, (tunnel_s[-2]-tunnel_s[0])/10)
+# Initial guesses for progress variable and its derivative.
+ocp.set_initial(s, np.linspace(s_grid[0], s_grid[-2], N+1))
+ocp.set_initial(sdot, (s_grid[-2] - s_grid[0])/10)
 
-ocp.subject_to(ocp.at_t0(s)==0)
+# Progress state variable s should start at 0.
+ocp.subject_to(ocp.at_t0(s) == 0)
 
+# Interpolant parameters;
+#     name      label for the resulting Function
+#     solver    name of the plugin (linear or bspline)
+#     grid    	collection of 1D grids whose outer product defines the full
+#                 N-D rectangular grid
+#     values	flattened vector of all values for all gridpoints
+#     options   linear:
+#                   - lookup_mode: Sets, for each grid dimenion, the lookup
+#                     algorithm used to find the correct index. 'linear' uses
+#                     a for-loop + break; 'exact' uses floored division (only
+#                     for uniform grids).
+#               bspline:
+#                   - algorithm: Algorithm used for fitting the data:
+#                     'not_a_knot' (default, same as Matlab), 'smooth_linear'.
+#                   - degree: Sets, for each grid dimension, the degree of the
+#                     spline.
+#                   - linear_solver: Solver used for constructing the
+#                     coefficient tensor.
+#                   - smooth_linear_frac: When 'smooth_linear' algorithm is
+#                     active, determines sharpness between 0 (sharp, as linear
+#                     interpolation) and 0.5 (smooth). Default value is 0.1.
 frac = 0.499
+spline_x = interpolant('x', 'bspline', [s_grid], x_grid,
+                       {"algorithm": "smooth_linear",
+                        "smooth_linear_frac": frac})
+spline_y = interpolant('y', 'bspline', [s_grid], y_grid,
+                       {"algorithm": "smooth_linear",
+                        "smooth_linear_frac": frac})
+spline_r = interpolant('r', 'bspline', [s_grid], r_grid,
+                       {"algorithm": "smooth_linear",
+                        "smooth_linear_frac": frac})
 
-spline_x = interpolant('x','bspline',[tunnel_s],tunnel_x,{"algorithm": "smooth_linear","smooth_linear_frac":frac})
-spline_y = interpolant('y','bspline',[tunnel_s],tunnel_y,{"algorithm": "smooth_linear","smooth_linear_frac":frac})
-spline_r = interpolant('r','bspline',[tunnel_s],tunnel_r,{"algorithm": "smooth_linear","smooth_linear_frac":frac})
+# Constrain the bicycle position (x, y) to be within a certain distance from
+# the centerline of a tunnel, defined by circles with radius spline_r(s)
+# around each point on the centerline given by spline_x(s) and spline_y(s).
+ocp.subject_to((x-spline_x(s))**2+(y-spline_y(s))**2 <= spline_r(s)**2)
 
-ocp.subject_to( (x-spline_x(s))**2+(y-spline_y(s))**2 <= spline_r(s)**2)
-
-# Minimal time
+# Go as far as possible (i.e. make s as large as possible).
 ocp.add_objective(-ocp.at_tf(s))
 
 # Pick a solution method
 ocp.solver('ipopt')
 
 # Make it concrete for this ocp
-ocp.method(MultipleShooting(N=N,M=4,intg='rk'))
+ocp.method(MultipleShooting(N=N, M=4, intg='rk'))
 
 # solve
 sol = ocp.solve()
 
-from pylab import *
+# Solution
 figure()
-
-ts, ss = sol.sample(s, grid='integrator',refine=10)
-ts = np.linspace(0,2*pi,1000)
+ts, ss = sol.sample(s, grid='control')
+ts = np.linspace(0, 2*pi, 1000)
 xs = np.array(spline_x(ss))
 ys = np.array(spline_y(ss))
 rs = np.array(spline_r(ss))
 for i in range(ss.shape[0]):
-  plot(xs[i]+rs[i]*cos(ts),ys[i]+rs[i]*sin(ts),'r-')
+    plot(xs[i]+rs[i]*cos(ts), ys[i]+rs[i]*sin(ts), 'r-')
+    plot(xs[i], ys[i], 'k.')
 
 ts, xs = sol.sample(x, grid='control')
 ts, ys = sol.sample(y, grid='control')
-
-plot(xs, ys,'bo')
+plot(xs, ys, 'bo')
 
 ts, xs = sol.sample(x, grid='integrator')
 ts, ys = sol.sample(y, grid='integrator')
-
 plot(xs, ys, 'b.')
 
-
-ts, xs = sol.sample(x, grid='integrator',refine=10)
-ts, ys = sol.sample(y, grid='integrator',refine=10)
-
+ts, xs = sol.sample(x, grid='integrator', refine=10)
+ts, ys = sol.sample(y, grid='integrator', refine=10)
 plot(xs, ys, '-')
-
-
-
-
 
 axis('equal')
 show(block=True)
