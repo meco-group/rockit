e26f573b9d9043bcdd522bee32a4484c3604e38e
Joris Gillis <joris.gillis42@gmail.com>
2024-06-03 14:27:11 +0200

    Model name

--- rockit/external/acados/method.py
+++ rockit/external/acados/method.py
@@ -927,7 +927,7 @@ class AcadosMethod(ExternalMethod):
                         after_init.write(f"""ocp_nlp_solver_opts_set(m->nlp_config, m->nlp_opts, "{k}","{v}");\n""")
                     elif isinstance(v, bool):
                         after_init.write(f"""int {k}={v};ocp_nlp_solver_opts_set(m->nlp_config, m->nlp_opts, "{k}",&{k});\n""")
-        assert subprocess.run(["cmake","-S", ".","-B", os.path.join(self.build_dir_abs,"build"),"-DCMAKE_BUILD_TYPE=Debug"], cwd=self.build_dir_abs).returncode==0
+        assert subprocess.run(["cmake","-S", ".","-B", os.path.join(self.build_dir_abs,"build"),"-DCMAKE_BUILD_TYPE=Debug", "-DMODEL_NAME="+ self.model_name], cwd=self.build_dir_abs).returncode==0
         assert subprocess.run(["cmake","--build",os.path.join(self.build_dir_abs,"build"),"--config","Debug"], cwd=self.build_dir_abs).returncode==0
         assert subprocess.run(["cmake","--install",os.path.join(self.build_dir_abs,"build"),"--prefix","."], cwd=self.build_dir_abs).returncode==0
         
